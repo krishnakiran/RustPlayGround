a78970ee394542cca673f8cdb2f0347c72c9b28f
Krishna Kiran <krishnakiran@users.noreply.github.com>
2020-02-10 23:32:14 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+//use ferris_says::say;
+//use std::io::{stdout,BufWriter};
+fn main() {
+   /* println!("Hello, world!");
+    let stdout = stdout();
+    let out = b"Hellow fellow Rustaceans!!";
+    let width=24;
+    let mut writer = BufWriter::new(stdout.lock());
+    say(out,width,&mut writer).unwrap();*/
+    let x=5;
+    let y=10;
+    let z=x+y;
+    println!("z is {}",z);
+    let ab=true;
+    println!("ab is {}",ab);
+}
